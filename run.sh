8629f50689d4966a6191d9c88d92579091ed29b6
Moritz R <c0derMo@users.noreply.github.com>
2021-11-12 21:43:53 +0100

    Try except the task execution to avoid loop-crashing & updated run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 cp ./namelessbot/config.json ./config.json
 
-rm -rf ./namelessbot/*
 git clone https://github.com/NamelessGroup/namelessbot.git
 
 cp ./config.json ./namelessbot/config.json
 
-apt-get install ffmpeg -Y
 pip install -r ./namelessbot/requirements.txt
 python ./namelessbot/main.py
