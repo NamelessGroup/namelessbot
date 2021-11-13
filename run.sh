b9210d4c2b2f8306b8b3c05721c40bc8d5a06180
Moritz R <c0derMo@users.noreply.github.com>
2021-11-13 17:37:14 +0100

    Update run.sh
    fix to new run.sh version (which is not even used serverside)

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 cp ./namelessbot/config.json ./config.json
 
+rm -rf ./namelessbot/*
 git clone https://github.com/NamelessGroup/namelessbot.git
 
 cp ./config.json ./namelessbot/config.json
 
+apt-get install ffmpeg -Y
 pip install -r ./namelessbot/requirements.txt
 python ./namelessbot/main.py
