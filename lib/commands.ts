43ff2f177c8da038b7af39647358f4a45ddbfb50
Moritz R <c0derMo@users.noreply.github.com>
2022-02-08 20:59:49 +0100

    Added missing commands file

---
+++ lib/commands.ts
@@ -0,0 +1,12 @@
+import test from "../messageCommands/test";
+import { IChatCommandList } from "../types";
+
+export const CHAT_COMMAND_PREFIX = "!";
+
+export const CHAT_COMMANDS: IChatCommandList = {
+    "test": test
+}
+
+export const SLASH_COMMANDS = {
+
+}
