b95d9816c1971d7c2e741552743ccb2d76648d17
Moritz R <c0derMo@users.noreply.github.com>
2022-02-10 11:52:57 +0100

    Reverted slashCommandRegister again

--- listeners/registerSlashCommands.ts
+++ listeners/registerSlashCommands.ts
@@ -1,13 +1,17 @@
 import {Client} from "discord.js";
 import {SLASH_COMMANDS} from "../lib/commands";
-
-const DIRECT_GUILD = "351806976713293826";
+import {get} from "../lib/configmanager";
 
 export default {
     event: 'ready',
     once: true,
     elevated: true,
     handler: async function(client: Client<true>) {
-        await client.application.commands.set(SLASH_COMMANDS.map((e) => e.command), DIRECT_GUILD);
+        const DIRECT_GUILD = get("default_guild", "config") as string;
+        if(DIRECT_GUILD === "") {
+            console.log("! No default_guild is set in 'config.json' - Slash commands can't be registered!");
+        } else {
+            await client.application.commands.set(SLASH_COMMANDS.map((e) => e.command), DIRECT_GUILD);
+        }
     }
 }
