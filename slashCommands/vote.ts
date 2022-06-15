814056c5f880a2878f772c76824e3995bc56db18
ujqlg <93595010+MartinKrausewitz@users.noreply.github.com>
2022-06-15 14:50:00 +0200

    voting function reworked

--- slashCommands/vote.ts
+++ slashCommands/vote.ts
@@ -2,6 +2,7 @@ import {CommandInteraction, Message, MessageEmbed, ReactionCollectorOptions} fro
 import {ISlashCommand} from "../types";
 import {ApplicationCommandOptionTypes} from "discord.js/typings/enums";
 import {get} from "../lib/configmanager";
+import {JSONOutput} from "typedoc";
 
 
 export default {
@@ -48,9 +49,10 @@ export default {
         let time = interaction.options.getInteger("votetime");
 
 
-        const timed = interaction.options.getBoolean("istimed");
+        let timed = interaction.options.getBoolean("istimed");
+        timed = (timed == null) ? false : timed;
 
-        let title = interaction.options.getString("name")
+        let title = interaction.options.getString("name");
 
         // --- Check for undefined
 
@@ -58,15 +60,19 @@ export default {
 
         title = (title == undefined) ? "" : title;
 
+        // --- group variables
+
+        const guid = get("vote_group", "config") as string;
+        const maingroup = interaction.guild.roles.cache.get(guid);
+
         // --- create initial system for time or group voting
 
         if (timed == true) {
             msg += "This is a timed vote. The vote is running " + time + " Seconds!";
-            maxLoop = 1
+            maxLoop = 1;
         } else {
             // get online member
-            const guid = get("vote_group", "config") as string;
-            const groupmembers = interaction.guild.roles.cache.get(guid).members.map(m=>m.user.id);
+            const groupmembers = maingroup.members.map(m=>m.user.id);
             usedVotes = Math.round(groupmembers.length/2);
             // further variables set
             usedVotes = ((usedVotes == 0) ? 1 : usedVotes);
@@ -79,7 +85,7 @@ export default {
             .setTitle((title == "") ? "Simple Voting ": title)
             .setDescription(msg)
             .setColor("#477ce0")
-            .addField("Max Time", "" + maxLoop * time + " Seconds", true);
+            .addField("Maximal Runtime", "" + maxLoop * time + " Seconds", true);
 
         const reply = await interaction.reply({embeds: [voteAN], fetchReply:true}) as Message;
 
@@ -87,17 +93,29 @@ export default {
         await reply.react(upEmo);       //react with emote up
         await reply.react(downEmo);     //react with emote down
 
+        const filter = (reaction, user) => { //filter for only getting the up and down emoji
+            return user.id != reply.author.id;
+        };
+
         //reaction controller
-        const collector = reply.createReactionCollector({
-            filter: (reaction, user) => { //filter for only getting the up and down emoji
-                return reaction.emoji.name === upEmo || reaction.emoji.name === downEmo && user.id != reply.author.id;
-            },
-            dispose: true} as ReactionCollectorOptions);
+        const collector = reply.createReactionCollector({filter, dispose: true} as ReactionCollectorOptions);
 
         //on reaction
         collector.on('collect', (reaction, user) => {
+            if (reaction.emoji.name != upEmo && reaction.emoji.name != downEmo) {
+                reaction.remove();
+            }
+            //console.log("collected")
+            for (const i of reactionAr) {
+                if (i[1] == user.username || reaction.message.member.roles.cache.some((role) => role.name === maingroup.name)) {
+                    reaction.users.remove(user);
+                    return;
+                }
+            }
             const pre = ((reaction.emoji.name === upEmo) ? 1 : -1);
-            reactionAr.push([pre, user.username])
+            reactionAr.push([pre, user.username]);
+
+            //console.log("saved");
         });
 
         //on removed reaction
@@ -114,16 +132,11 @@ export default {
         });
 
         //delay loop
-        for (let i = 0; i < maxLoop; i++) {
-            await delay(time * 1000);
+        for (let i = 0; i < maxLoop * time; i++) {
+            await delay(1000);
             //counting the votes a user could vote up and down
-            const voteCounter = Array(0);
-            for (const i of reactionAr) {
-                if (voteCounter.indexOf(i[1]) == -1) {
-                    voteCounter.push(i[1]);
-                }
-            }
-            if (voteCounter.length >= usedVotes && timed == false) {
+            //console.log("Test: " + reactionAr.length + ";" + usedVotes + ";" + timed);
+            if (reactionAr.length >= usedVotes && timed == false) {
                 voteAN.addField("Enough Votes", "Vote ending in " + time + " Seconds");
                 await reply.edit({embeds: [voteAN]});
                 await delay(time * 1000);
@@ -136,50 +149,54 @@ export default {
 
 
         //counting up and down votes and creating the text
-        let upVotes = "";
-        let downVotes = "";
-
-        let up = 0;
-        let down = 0;
-
-        for (const i of reactionAr) {
-            if (i[0] == -1) {
-                downVotes += ("🔴 " + i[1] + "\n");
-                down += 1;
-            } else {
-                upVotes += ("🟢 " + i[1] + "\n");
-                up += 1;
-            }
-        }
-
-        upVotes = (upVotes == "") ? "None" : upVotes;
-        downVotes = (downVotes == "") ? "None" : downVotes;
-
-        //remove all reactions
-
-        await reply.reactions.removeAll();
-
-        //finished embed
-
-        const msgEmbed = new MessageEmbed()
-            .setColor((down < up) ? "#02B22E" : "#cc0000")
-            .setTitle((down < up) ? "Vote accepted" : "Vote failed")
-            .addField("For", upVotes, true)
-            .addField("Against", downVotes, true)
-            .setTimestamp();
-
-        //if a title were specified it will be displayed
-
-        if (title != "") {
-            msgEmbed.setDescription(title);
-        }
-
-        //show embed
-        await reply.edit({embeds:[msgEmbed]})
+        await printVotes(reactionAr, reply, title)
     }
 } as ISlashCommand;
 
 //delays a function
 function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function printVotes(reactionAr, reply, title) {
+    let upVotes = "";
+    let downVotes = "";
+
+    let up = 0;
+    let down = 0;
+
+    for (const i of reactionAr) {
+        if (i[0] == -1) {
+            downVotes += ("🔴 " + i[1] + "\n");
+            down += 1;
+        } else {
+            upVotes += ("🟢 " + i[1] + "\n");
+            up += 1;
+        }
+    }
+
+    upVotes = (upVotes == "") ? "None" : upVotes;
+    downVotes = (downVotes == "") ? "None" : downVotes;
+
+    //remove all reactions
+
+    await reply.reactions.removeAll();
+
+    //finished embed
+
+    const msgEmbed = new MessageEmbed()
+        .setColor((down < up) ? "#02B22E" : "#cc0000")
+        .setTitle((down < up) ? "Vote accepted" : "Vote failed")
+        .addField("For", upVotes, true)
+        .addField("Against", downVotes, true)
+        .setTimestamp();
+
+    //if a title were specified it will be displayed
+
+    if (title != "") {
+        msgEmbed.setDescription(title);
+    }
+
+    //show embed
+    await reply.edit({embeds:[msgEmbed]})
 }
