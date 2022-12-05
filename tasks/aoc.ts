59c3e318c8e70fb6ea493fad67ae403496f23747
ujqlg <93595010+MartinKrausewitz@users.noreply.github.com>
2022-12-05 14:25:39 +0100

    Merge Advent of Code Leaderboard to production server (#26)
    * Advent of Code Leaderboard

---
+++ tasks/aoc.ts
@@ -0,0 +1,10 @@
+import {Client, TextChannel} from "discord.js";
+import {TaskExecutor} from "../types";
+import {embedLeaderboard} from "../lib/aocleaderboardparser"
+import {get} from "../lib/configmanager";
+
+export default (async (client: Client) => {
+    const embed = await embedLeaderboard(get("id", "aoc.json") as number, get("year", "aoc.json") as number);
+    const channel = await client.channels.fetch(get('announcement_channel', 'config') as string) as TextChannel;
+    await channel.send({ embeds: [embed] });
+}) as TaskExecutor
