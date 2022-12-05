100b98244d75cb6647c5518e663aa5891b7b5fdb
ujqlg <93595010+MartinKrausewitz@users.noreply.github.com>
2022-12-05 22:21:44 +0100

    Fixed crash

--- tasks/aoc.ts
+++ tasks/aoc.ts
@@ -4,7 +4,7 @@ import {embedLeaderboard} from "../lib/aocleaderboardparser"
 import {get} from "../lib/configmanager";
 
 export default (async (client: Client) => {
-    const embed = await embedLeaderboard(get("id", "aoc.json") as number, get("year", "aoc.json") as number);
+    const embed = await embedLeaderboard(get("id", "aoc") as number, get("year", "aoc") as number);
     const channel = await client.channels.fetch(get('announcement_channel', 'config') as string) as TextChannel;
     await channel.send({ embeds: [embed] });
 }) as TaskExecutor
