2892208625c0273b13caf45e4a0b62650bb3355e
Alexander Vogt <alexander_vogt@outlook.de>
2022-12-18 18:17:36 +0100

    set reccuring task to evening on the day before

--- lib/tasks.ts
+++ lib/tasks.ts
@@ -26,9 +26,9 @@ export const TASKS = [
     new RecurringTask(Weekday.SUNDAY, 22, 5, aoc),
 
     // Attendance-Tracker
-    new RecurringTask(Weekday.MONDAY, 6, 0, attendanceTracker, [Weekday.MONDAY]),
-    new RecurringTask(Weekday.TUESDAY, 6, 0, attendanceTracker, [Weekday.TUESDAY]),
-    new RecurringTask(Weekday.WEDNESDAY, 6, 0, attendanceTracker, [Weekday.WEDNESDAY]),
-    new RecurringTask(Weekday.THURSDAY, 6, 0, attendanceTracker, [Weekday.THURSDAY]),
-    new RecurringTask(Weekday.FRIDAY, 6, 0, attendanceTracker, [Weekday.FRIDAY]),
+    new RecurringTask(Weekday.SUNDAY, 20, 0, attendanceTracker, [Weekday.MONDAY]),
+    new RecurringTask(Weekday.MONDAY, 20, 0, attendanceTracker, [Weekday.TUESDAY]),
+    new RecurringTask(Weekday.TUESDAY, 20, 0, attendanceTracker, [Weekday.WEDNESDAY]),
+    new RecurringTask(Weekday.WEDNESDAY, 20, 0, attendanceTracker, [Weekday.THURSDAY]),
+    new RecurringTask(Weekday.THURSDAY, 20, 0, attendanceTracker, [Weekday.FRIDAY]),
 ]
