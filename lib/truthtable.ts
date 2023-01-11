2d075985e7d3cd96de01c02f0a33943946d0a9a3
Moritz R <c0derMo@users.noreply.github.com>
2023-01-11 14:53:58 +0100

    Documenting lib/truthtable.ts

--- lib/truthtable.ts
+++ lib/truthtable.ts
@@ -5,111 +5,275 @@
  * Code stolen to a large amount and just adopted & typescript-ified
  */
 
-//region AST Classes
+
+//region Nodes
+/**
+ * Interface for representing nodes
+ */
 interface Node {
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     */
     evaluate(assignment: boolean[]): boolean;
+    /**
+     * Returning a string version of the node
+     */
     toString(variables: string[]): string;
 }
 
+/**
+ * Node representing a truthy value
+ */
 class TrueNode implements Node {
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     * 
+     * @param assignment Variable input assignment
+     * @returns Boolean value of this node
+     */
     evaluate(assignment: boolean[]): boolean {
         return true;
     }
+    /**
+     * Returning a string version of the node
+     * 
+     * @param variables Variable input assignment
+     * @returns Boolean value of this node
+     */
     toString(variables: string[]): string {
         return "T";
     }
 }
 
+/**
+ * Node representing a falsy value
+ */
 class FalseNode implements Node {
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     * 
+     * @param assignment Variable input assignment
+     * @returns Boolean value of this node
+     */
     evaluate(assignment: boolean[]): boolean {
         return false;
     }
+    /**
+     * Returning a string version of the node
+     * 
+     * @param variables Variable input assignment
+     * @returns Boolean value of this node
+     */
     toString(variables: string[]): string {
         return "F";
     }
 }
 
+/**
+ * Node inverting the underlying value
+ */
 class NegateNode implements Node {
     private underlying: Node;
+    /**
+     * Creating a NegateNode supplying an underlying node
+     * 
+     * @param underlying Underlying node
+     */
     constructor(underlying: Node) {
         this.underlying = underlying;
     }
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     * 
+     * @param assignment Variable input assignment
+     * @returns Boolean value of this node
+     */
     evaluate(assignment: boolean[]): boolean {
         return !this.underlying.evaluate(assignment);
     }
+    /**
+     * Returning a string version of the node
+     * 
+     * @param variables Variable input assignment
+     * @returns Boolean value of this node
+     */
     toString(variables: string[]): string {
         return "!" + this.underlying.toString(variables);
     }
 }
 
+/**
+ * Node representing an AND gate
+ */
 class AndNode implements Node {
     private lhs: Node;
     private rhs: Node;
+    /**
+     * Creating an AndNode with the supplied lefthand- and righthand-expressions
+     * 
+     * @param lhs Lefthand-expression
+     * @param rhs Righthand-expression
+     */
     constructor(lhs: Node, rhs: Node) {
         this.lhs = lhs;
         this.rhs = rhs;
     }
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     * 
+     * @param assignment Variable input assignment
+     * @returns Boolean value of this node
+     */
     evaluate(assignment: boolean[]): boolean {
         return this.lhs.evaluate(assignment) && this.rhs.evaluate(assignment);
     }
+    /**
+     * Returning a string version of the node
+     * 
+     * @param variables Variable input assignment
+     * @returns Boolean value of this node
+     */
     toString(variables: string[]): string {
         return "(" + this.lhs.toString(variables) + " & " + this.rhs.toString(variables) + ")";
     }
 }
 
+/**
+ * Node representing an OR gate
+ */
 class OrNode implements Node {
     private lhs: Node;
     private rhs: Node;
+    /**
+     * Creating an OrNode with the supplied lefthand- and righthand-expressions
+     * 
+     * @param lhs Lefthand-expression
+     * @param rhs Righthand-expression
+     */
     constructor(lhs: Node, rhs: Node) {
         this.lhs = lhs;
         this.rhs = rhs;
     }
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     * 
+     * @param assignment Variable input assignment
+     * @returns Boolean value of this node
+     */
     evaluate(assignment: boolean[]): boolean {
         return this.lhs.evaluate(assignment) || this.rhs.evaluate(assignment);
     }
+    /**
+     * Returning a string version of the node
+     * 
+     * @param variables Variable input assignment
+     * @returns Boolean value of this node
+     */
     toString(variables: string[]): string {
         return "(" + this.lhs.toString(variables) + " | " + this.rhs.toString(variables) + ")";
     }
 }
 
+/**
+ * Node representing an implication
+ */
 class ImpliesNode implements Node {
     private lhs: Node;
     private rhs: Node;
+    /**
+     * Creating an ImpliesNode with the supplied lefthand- and righthand-expressions
+     * 
+     * @param lhs Lefthand-expression
+     * @param rhs Righthand-expression
+     */
     constructor(lhs: Node, rhs: Node) {
         this.lhs = lhs;
         this.rhs = rhs;
     }
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     * 
+     * @param assignment Variable input assignment
+     * @returns Boolean value of this node
+     */
     evaluate(assignment: boolean[]): boolean {
         return !this.lhs.evaluate(assignment) || this.rhs.evaluate(assignment);
     }
+    /**
+     * Returning a string version of the node
+     * 
+     * @param variables Variable input assignment
+     * @returns Boolean value of this node
+     */
     toString(variables: string[]): string {
         return "(" + this.lhs.toString(variables) + " -> " + this.rhs.toString(variables) + ")";
     }
 }
 
+/**
+ * Node representing an equivalence / iff
+ */
 class IffNode implements Node {
     private lhs: Node;
     private rhs: Node;
+    /**
+     * Creating an IffNode with the supplied lefthand- and righthand-expressions
+     * 
+     * @param lhs Lefthand-expression
+     * @param rhs Righthand-expression
+     */
     constructor(lhs: Node, rhs: Node) {
         this.lhs = lhs;
         this.rhs = rhs;
     }
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     * 
+     * @param assignment Variable input assignment
+     * @returns Boolean value of this node
+     */
     evaluate(assignment: boolean[]): boolean {
         return this.lhs.evaluate(assignment) === this.rhs.evaluate(assignment);
     }
+    /**
+     * Returning a string version of the node
+     * 
+     * @param variables Variable input assignment
+     * @returns Boolean value of this node
+     */
     toString(variables: string[]): string {
         return "(" + this.lhs.toString(variables) + " <-> " + this.rhs.toString(variables) + ")";
     }
 }
 
+/**
+ * Node representing a variable input
+ */
 class VariableNode implements Node {
     private readonly index: number;
+    /**
+     * Creating a VariableNode with the supplied variable index
+     * 
+     * @param index Variable index
+     */
     constructor(index: number) {
         this.index = index;
     }
+    /**
+     * Evaulating this node, returning it's value with the corresponding variable input
+     * 
+     * @param assignment Variable input assignment
+     * @returns Boolean value of this node
+     */
     evaluate(assignment: boolean[]): boolean {
         return assignment[this.index];
     }
+    /**
+     * Returning a string version of the node
+     * 
+     * @param variables Variable input assignment
+     * @returns Boolean value of this node
+     */
     toString(variables: string[]): string {
         return variables[this.index];
     }
@@ -132,8 +296,14 @@ interface ScanResult {
     variables: string[];
 }
 
-const EOF_TOKEN = "$"
+const EOF_TOKEN = "$";
 
+/**
+ * Scanning the input string, building variables & tokens
+ * 
+ * @param input Input string to scan
+ * @returns Scan result
+ */
 function scan(input: string): ScanResult {
 
     // Check for illegal characters
@@ -159,10 +329,7 @@ function scan(input: string): ScanResult {
 
         if (curr === EOF_TOKEN) {
             tokens.push(makeIdentityToken(curr, i));
-            return transformVariableSet({
-                tokens,
-                variableSet
-            })
+            break;
         } else {
             const variable = readVariable(input, i);
             const operator = readOperator(input, i);
@@ -180,26 +347,51 @@ function scan(input: string): ScanResult {
             }
         }
     }
-
+    return transformVariableSet({
+        tokens,
+        variableSet
+    });
 }
 
+/**
+ * Creates an IdentityToken from the supplied string & index
+ *
+ * @param str String representation of the token
+ * @param index Starting index of the token
+ * @returns IdentityToken of the string & index
+ */
 function makeIdentityToken(str: string, index: number): Token {
     return {
         type: translate(str),
         start: index,
         end: index + str.length
-    }
+    };
 }
 
+/**
+ * Creates an variable token with the supplied index & length
+ * 
+ * @param varIndex Variable index
+ * @param start Start of the variable
+ * @param end End of the variable
+ * @returns VariableToken
+ */
 function makeVariableToken(varIndex: string, start: number, end: number): Token {
     return {
         type: "variable",
         index: varIndex,
         start,
         end
-    }
+    };
 }
 
+/**
+ * Tries to read a variable starting at the supplied index
+ * 
+ * @param str Input string
+ * @param index Starting index
+ * @returns Variable name, if valid, null otherwise
+ */
 function readVariable(str: string, index: number): string {
     if (!/[a-z_]/.test(str.charAt(index))) {
         return null;
@@ -214,6 +406,13 @@ function readVariable(str: string, index: number): string {
     return result;
 }
 
+/**
+ * Tries to read an operator starting at the supplied index
+ * 
+ * @param str Input string
+ * @param index Starting index
+ * @returns Operator, if valid, null otherwise
+ */
 function readOperator(str: string, index: number): string {
     // Eight-char operators
     if (index < str.length - 7) {
@@ -264,6 +463,12 @@ function readOperator(str: string, index: number): string {
     return null;
 }
 
+/**
+ * Translating operators with multiple options into one string representation
+ * 
+ * @param input String to translate
+ * @returns String representation of the operator represented by the input
+ */
 function translate(input: string): string {
     if (input === "/\\" || input === "&&" || input === "and") return "&";
     if (input === "\\/" || input === "||" || input === "or") return "|";
@@ -279,10 +484,22 @@ function translate(input: string): string {
     return input;
 }
 
+/**
+ * Tests, if the input is whitespace
+ * 
+ * @param str String to test
+ * @returns true, if input is whitespace
+ */
 function isWhitespace(str: string): boolean {
     return /\s/.test(str);
 }
 
+/**
+ * Adds proper variable tokens & indexes to the preliniary scan result
+ * 
+ * @param preliminary Preliniary scan result
+ * @returns Scan result with proper variable indexes
+ */
 function transformVariableSet(preliminary: PreliniaryScanResult): ScanResult {
     const variables = [];
     for (const key in preliminary.variableSet) {
@@ -305,7 +522,7 @@ function transformVariableSet(preliminary: PreliniaryScanResult): ScanResult {
     return {
         tokens: preliminary.tokens,
         variables
-    }
+    };
 }
 //endregion
 
@@ -315,6 +532,12 @@ interface ParserResult {
     variables: string[];
 }
 
+/**
+ * Parsing the input string, building an AST of the boolean expression
+ * 
+ * @param input Input string
+ * @returns AST & variables of the input expression
+ */
 export function parse(input: string): ParserResult {
     const scanResult = scan(input);
     const tokens = scanResult.tokens;
@@ -396,7 +619,7 @@ export function parse(input: string): ParserResult {
                 const expr = operands.pop();
                 addOperand(expr, operands, operators);
             } else {
-                throw new Error("Missing closing parenthesis or binary operator.")
+                throw new Error("Missing closing parenthesis or binary operator.");
             }
         }
     }
@@ -413,9 +636,16 @@ export function parse(input: string): ParserResult {
     return {
         ast: operands.pop(),
         variables: scanResult.variables
-    }
+    };
 }
 
+/**
+ * Adds node to the list of operands, applying negations as needed
+ * 
+ * @param node Node to add
+ * @param operands Current operands
+ * @param operators Current operators
+ */
 function addOperand(node: Node, operands: Node[], operators: Token[]): void {
     while (operators.length > 0 && topOf(operators).type === "!") {
         operators.pop();
@@ -424,12 +654,24 @@ function addOperand(node: Node, operands: Node[], operators: Token[]): void {
     operands.push(node);
 }
 
+/**
+ * Checks if the input token is a valid operand
+ * 
+ * @param token Input token
+ * @returns true, if token is truthy value, falsy value or variable
+ */
 function isOperand(token: Token): boolean {
     return token.type === "T" ||
         token.type === "F" ||
         token.type === "variable";
 }
 
+/**
+ * Returns a node of the input token type
+ * 
+ * @param token Token to transform
+ * @returns Node of the input token
+ */
 function wrapOperand(token: Token): Node {
     if (token.type === "T") return new TrueNode();
     if (token.type === "F") return new FalseNode();
@@ -437,6 +679,12 @@ function wrapOperand(token: Token): Node {
     throw new Error(`Unreachable code error: Token ${token.type} isn't an operand.`);
 }
 
+/**
+ * Checks if the input token represents a binary operator (e.g. an operator that needs two operands)
+ * 
+ * @param token Token to check
+ * @returns true, if the input token is a binary operator
+ */
 function isBinaryOperator(token: Token): boolean {
     return token.type === "<->" ||
         token.type === "->" ||
@@ -448,6 +696,19 @@ function isBinaryOperator(token: Token): boolean {
         token.type === "!<->";
 }
 
+/**
+ * Returns the priority of the supplied token
+ * 
+ * Token priorities:
+ * END_OF_FILE = -1
+ * Iff / NotIff = 0
+ * Implies / NotImplies = 1
+ * Or / Nor = 2
+ * And / Nand = 3
+ * 
+ * @param token Token to check
+ * @returns Priority of the token
+ */
 function priorityOf(token: Token): number {
     if (token.type === EOF_TOKEN) return -1;
     if (token.type === "<->" || token.type === "!<->") return 0;
@@ -457,6 +718,14 @@ function priorityOf(token: Token): number {
     throw new Error(`Unreachable code error: Should never need the priority of ${token.type}`);
 }
 
+/**
+ * Builds an operator node of a binary operator with both operands
+ * 
+ * @param lhs Lefthand-expression as node
+ * @param token Token to build node with
+ * @param rhs Righthand-expression as node
+ * @returns Built node
+ */
 function createOperatorNode(lhs: Node, token: Token, rhs: Node): Node {
     if (token.type === "<->") return new IffNode(lhs, rhs);
     if (token.type === "->") return new ImpliesNode(lhs, rhs);
@@ -469,6 +738,12 @@ function createOperatorNode(lhs: Node, token: Token, rhs: Node): Node {
     throw new Error(`Unreachable code error: Should never create an operator node from ${token.type}`);
 }
 
+/**
+ * Returns the top/last element of an array without removing it
+ * 
+ * @param array Input array
+ * @returns Top/last element of that array
+ */
 function topOf<Type>(array: Type[]): Type {
     if (array.length === 0) {
         throw new Error("Cant get the top of an empty array.");
@@ -483,6 +758,12 @@ interface TruthTable {
     results: { assignment: boolean[]; result: boolean; }[];
 }
 
+/**
+ * Generates a truthtable with the supplied AST & variables
+ * 
+ * @param parseResult Parsed truthtable
+ * @returns Truthtable with variables & results for given variable assignments
+ */
 export function generateTruthTable(parseResult: ParserResult): TruthTable {
     const results = [];
 
@@ -498,9 +779,16 @@ export function generateTruthTable(parseResult: ParserResult): TruthTable {
     return {
         variables: parseResult.variables,
         results
-    }
+    };
 }
 
+/**
+ * Returns the next variable assignment flipping the rightmost false-value to true and all values right from that to false.
+ * Mutates the input assignment
+ * 
+ * @param assignment Previous assignment
+ * @returns true, if there is a next asssignment, false otherwise
+ */
 function nextAssignment(assignment: boolean[]): boolean {
     let flipIndex = assignment.length - 1;
     while (flipIndex >= 0 && assignment[flipIndex]) flipIndex--;
