32e905d6082db85d03a7dab56ee850553342b617
Moritz R <c0derMo@users.noreply.github.com>
2023-02-20 10:54:22 +0100

    Removed mensa & attendance tracker from running tasks

--- lib/registry.ts
+++ lib/registry.ts
@@ -8,9 +8,9 @@ import mensaCommand from "../modules/mensa/mensaCommand";
 import truthtableCommand from "../modules/truthtable/truthtableCommand";
 import voteCommand from "../modules/vote/voteCommand";
 
-import mensaTask from "../modules/mensa/mensaTask";
+// import mensaTask from "../modules/mensa/mensaTask";
 import aocTask from "../modules/aoc/aocTask";
-import attendanceTrackerTask from "../modules/attendancetracker/attendanceTrackerTask";
+// import attendanceTrackerTask from "../modules/attendancetracker/attendanceTrackerTask";
 import koeriSelectionMenu from "../modules/koeri/koeriSelectionMenuListener";
 import attendanceTrackerButton from "../modules/attendancetracker/attendanceTrackerButtonListener";
 
@@ -20,11 +20,14 @@ export const SLASH_COMMANDS: ISlashCommand[] = [
 
 export const TASKS = [
     // Mensa-Plans
-    new RecurringTask(Weekday.MONDAY, 9, 48, mensaTask),
-    new RecurringTask(Weekday.TUESDAY, 9, 48, mensaTask),
-    new RecurringTask(Weekday.WEDNESDAY, 9, 48, mensaTask),
-    new RecurringTask(Weekday.THURSDAY, 9, 48, mensaTask),
-    new RecurringTask(Weekday.FRIDAY, 9, 48, mensaTask),
+    
+    /*
+     * new RecurringTask(Weekday.MONDAY, 9, 48, mensaTask),
+     * new RecurringTask(Weekday.TUESDAY, 9, 48, mensaTask),
+     * new RecurringTask(Weekday.WEDNESDAY, 9, 48, mensaTask),
+     * new RecurringTask(Weekday.THURSDAY, 9, 48, mensaTask),
+     * new RecurringTask(Weekday.FRIDAY, 9, 48, mensaTask),
+     */
 
     // Advent of Code
     new RecurringTask(Weekday.MONDAY, 22, 5, aocTask),
@@ -36,11 +39,14 @@ export const TASKS = [
     new RecurringTask(Weekday.SUNDAY, 22, 5, aocTask),
 
     // Attendance-Tracker
-    new RecurringTask(Weekday.SUNDAY, 20, 0, attendanceTrackerTask, [Weekday.MONDAY]),
-    new RecurringTask(Weekday.MONDAY, 20, 0, attendanceTrackerTask, [Weekday.TUESDAY]),
-    new RecurringTask(Weekday.TUESDAY, 20, 0, attendanceTrackerTask, [Weekday.WEDNESDAY]),
-    new RecurringTask(Weekday.WEDNESDAY, 20, 0, attendanceTrackerTask, [Weekday.THURSDAY]),
-    new RecurringTask(Weekday.THURSDAY, 20, 0, attendanceTrackerTask, [Weekday.FRIDAY]),
+    
+    /*
+     * new RecurringTask(Weekday.SUNDAY, 20, 0, attendanceTrackerTask, [Weekday.MONDAY]),
+     * new RecurringTask(Weekday.MONDAY, 20, 0, attendanceTrackerTask, [Weekday.TUESDAY]),
+     * new RecurringTask(Weekday.TUESDAY, 20, 0, attendanceTrackerTask, [Weekday.WEDNESDAY]),
+     * new RecurringTask(Weekday.WEDNESDAY, 20, 0, attendanceTrackerTask, [Weekday.THURSDAY]),
+     * new RecurringTask(Weekday.THURSDAY, 20, 0, attendanceTrackerTask, [Weekday.FRIDAY]),
+     */
 ];
 
 export const LISTENERS: IEventListener[] = [
