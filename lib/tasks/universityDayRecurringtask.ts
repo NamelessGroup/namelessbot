7b27ae12b99f44ee059d6c91a5bbe9b72f4f5efb
Alexander Vogt <alexander_vogt@outlook.de>
2023-03-15 19:51:42 +0100

    implemented lookahead

--- lib/tasks/universityDayRecurringtask.ts
+++ lib/tasks/universityDayRecurringtask.ts
@@ -1,7 +1,8 @@
-import {RecurringTask} from "./recurringtask";
+import {RecurringTask, Weekday} from "./recurringtask";
 import {DateTime} from "luxon";
 import KitDateParser from "./KitDateParser";
 import {isHoliday} from "feiertagejs/build/feiertage.js";
+import {TaskExecutor} from "../../types";
 
 /**
  * Special kind of Recurring Task that only gets executed in weeks, where lectures are held
@@ -9,17 +10,34 @@ import {isHoliday} from "feiertagejs/build/feiertage.js";
 export default class UniversityDayRecurringTask extends RecurringTask {
 
     private static kitDates = new KitDateParser();
+    private lookAheadDays: number;
+
+    /**
+     * Creates a new recurring task, which will always get run at the specified day & time and when the day has lectures.
+     *
+     * @param weekday Weekday on which the task should run
+     * @param hour Hour which the task should run
+     * @param minute Minute which the task should run
+     * @param runner Function to execute
+     * @param functionArguments Array of additional parameters to pass to the runner function
+     * @param lookAheadDays Days to check in advance for being a day with lectures
+     */
+    constructor(weekday: Weekday, hour: number, minute: number, runner: TaskExecutor, functionArguments?: unknown[], lookAheadDays?: number) {
+        super(weekday, hour, minute, runner, functionArguments);
+        if (lookAheadDays) {
+            this.lookAheadDays = lookAheadDays;
+        } else {
+            this.lookAheadDays = 0;
+        }
+    }
 
     public override shouldRunAtTime(time: DateTime): boolean {
         if (!super.shouldRunAtTime(time)) {
             return false;
         }
 
-        const jsTime = time.toJSDate();
-
-        //getLanguage();
-        return UniversityDayRecurringTask.kitDates.isLectureTime(jsTime) && !isHoliday(jsTime, "BW");
-
+        const addedTime = time.plus({days: this.lookAheadDays});
+        return UniversityDayRecurringTask.kitDates.isLectureTime(addedTime) && !isHoliday(addedTime.toJSDate(), "BW");
     }
 
 }
