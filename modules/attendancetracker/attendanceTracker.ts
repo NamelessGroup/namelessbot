0de8a739351ea6276b1e11769731ded840eafac4
Moritz R <c0derMo@users.noreply.github.com>
2023-04-19 10:18:04 +0200

    All blocks are written to the file once the task is executed, fixed whitespace issues

--- modules/attendancetracker/attendanceTracker.ts
+++ modules/attendancetracker/attendanceTracker.ts
@@ -42,7 +42,7 @@ export function getBlocks(weekday?: Weekday, includeAttendence?: boolean, includ
         const filteredBlocks = allBlocks.filter(e => { return e.weekday == weekday; });
         if (includeAttendence) {
             return filteredBlocks.map(e => {
-                return Object.assign(e, { attendance: attendanceMap[e.title.toLowerCase().replace(/\s/, "_")] });
+                return Object.assign(e, { attendance: attendanceMap[e.title.toLowerCase().replace(/\s/g, "_")] });
             });
         }
         return filteredBlocks.sort(sortBlocks);
@@ -171,8 +171,23 @@ export function resetAttendance(): void {
  * @param userId User to update attendance for
  */
 async function updateAttendanceFile(weekday: Weekday, block: string, userId: string): Promise<void> {
-    const key = getNextTime(weekday, 0, 0).toISO() + "-" + block;
+    const key = getNextTime(weekday, 0, 0).toISODate() + "-" + block;
     const fileContent = get(key, "attendance") as Record<string, boolean>;
     fileContent[userId] = !fileContent[userId];
     await write(key, "attendance", fileContent);
+}
+
+/**
+ * Adds all blocks to the attendance tracker file.
+ * 
+ * @param weekday Weekday to add blocks for
+ * @param blocks Blocks to add
+ */
+export async function writeAllBlocksToAttendanceFile(weekday: Weekday, blocks: CalendarBlock[]): Promise<void> {
+    for (const block of blocks) {
+        const key = getNextTime(weekday, 0, 0).toISODate() + "-" + block.title.toLowerCase().replace(/\s/g, "_");
+        if (get(key, "attendance") === undefined) {
+            await write(key, "attendance", {});
+        }
+    }
 }
--- modules/attendancetracker/attendanceTrackerTask.ts
+++ modules/attendancetracker/attendanceTrackerTask.ts
@@ -3,7 +3,7 @@ import { get } from "../../lib/configmanager";
 import { Weekday } from "../../lib/tasks/recurringtask";
 import {TaskExecutor} from "../../types";
 import {buildAttendanceAction, buildTimeTableEmbed} from "./attendanceTrackerVisuals";
-import {getBlocks, resetAttendance} from "./attendanceTracker";
+import {getBlocks, resetAttendance, writeAllBlocksToAttendanceFile} from "./attendanceTracker";
 
 /**
  * TaskExecutor for printing the timetable with attendance buttons of the given weekday <br>
@@ -15,6 +15,7 @@ export default (async (client: Client, weekday: Weekday) => {
     resetAttendance();
     const channel = await client.channels.fetch(get('announcement_channel', 'config') as string) as TextChannel;
     const blocks = getBlocks(weekday, true);
+    await writeAllBlocksToAttendanceFile(weekday, blocks);
     await channel.send({embeds: [buildTimeTableEmbed(blocks, weekday)],
                         components: buildAttendanceAction(blocks)});
 }) as TaskExecutor;
