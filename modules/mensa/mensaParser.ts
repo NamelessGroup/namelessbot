dfb89e07d54be9d69b7bba31e198ebb99c2b87df
Alexander Vogt <alexander_vogt@outlook.de>
2023-05-19 10:08:51 +0200

    Added emojis

--- modules/mensa/mensaParser.ts
+++ modules/mensa/mensaParser.ts
@@ -29,9 +29,9 @@ export function buildMensaEmbed(foodPlan: CanteenLine[]): EmbedBuilder {
         if (line.meals.length > 0) {
             const content = line.meals.map(e => {
                 if (e.price !== "") {
-                    return `${e.name} (${e.price})`;
+                    return `${getClassificationEmoji(e.classifiers)} ${e.name} (${e.price})`;
                 } else {
-                    return `_${e.name}_`;
+                    return `${getClassificationEmoji(e.classifiers)} _${e.name}_`;
                 }
             }).join("\n");
             embed.addFields({name: line.name, value: content, inline: true});
@@ -44,4 +44,29 @@ export function buildMensaEmbed(foodPlan: CanteenLine[]): EmbedBuilder {
     }
 
     return embed;
+}
+
+/**
+ * Generates the emoji representation of the given classifiers of a meal
+ *
+ * @param classification Classifiers of meal
+ * @returns Emoji of the classifiers
+ */
+function getClassificationEmoji(classification: string[]): string {
+    if (classification.length == 0) {
+        return '';
+    }
+    if (classification.includes('VG')) {
+        return ':ear_of_rice:';
+    }
+    if (classification.includes('VEG')) {
+        return ':carrot:';
+    }
+    if (classification.includes('S') || classification[0].startsWith('S')) {
+        return ':pig:';
+    }
+    if (classification.includes('R') || classification[0].startsWith('R')) {
+        return ':cow:';
+    }
+    return '';
 }
